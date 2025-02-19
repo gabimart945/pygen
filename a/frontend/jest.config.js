617e048d3b3e3888eea9dcd250708c403ff90a57
Gabi Martinez Pieper <gabrielmartinez945@comunidadunir.net>
2025-02-19 14:12:43 +0100

    test

--- a/frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-
-        module.exports = {
-          testEnvironment: 'jsdom',
-          moduleNameMapper: {
-            '\.(css|less|scss|sass)$': 'identity-obj-proxy',
-          },
-          setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-        };
-        
--- a/frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api",
-});
-
-
-
-export default api;
